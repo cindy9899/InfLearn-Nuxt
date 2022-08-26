e0740c365b581aa485d490dd9150b5d69acfcc7a
cindy9899 <yeonhoo9899@gmail.com>
2022-08-26 20:02:35 +0900

    Set Json server

---
+++ pages/product.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ul>
+      <li>상품 1</li>
+      <li>상품 2</li>
+      <li>상품 3</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
