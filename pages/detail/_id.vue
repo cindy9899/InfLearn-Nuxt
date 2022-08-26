f736c4bcf41f5d3782d84e37ae8a144d4432165c
cindy9899 <yeonhoo9899@gmail.com>
2022-08-27 06:18:51 +0900

    detail page meta info changed

--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -24,6 +24,19 @@ import {fetchProductById, createCartItem} from '@/api/index'
       const product = response.data
       return {product}
     },
+    head: {
+    title: 'Shopping Item Detail',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '이 상품은 ~~입니다.' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
     methods: {
       async addToCart(){
         const response = await createCartItem(this.product);
